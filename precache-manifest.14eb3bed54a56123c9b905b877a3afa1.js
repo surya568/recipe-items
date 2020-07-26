cd2cfc2d1f42839fe8b576326fc6a3b680736568
surya568 <17pa1a0568@vishnu.edu.in>
2020-07-26 20:45:41 +0530

    Updates

---
+++ precache-manifest.14eb3bed54a56123c9b905b877a3afa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7008c2796ee82d53e7a372e44dbcd09f",
+    "url": "/recipe-items/index.html"
+  },
+  {
+    "revision": "03c19ea110d21542bc15",
+    "url": "/recipe-items/static/css/main.886fce39.chunk.css"
+  },
+  {
+    "revision": "410008c80abd6bf7e93c",
+    "url": "/recipe-items/static/js/2.ae763374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-items/static/js/2.ae763374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c19ea110d21542bc15",
+    "url": "/recipe-items/static/js/main.c4a0d2e2.chunk.js"
+  },
+  {
+    "revision": "35c5a4de2bf973bf951d",
+    "url": "/recipe-items/static/js/runtime-main.70861b74.js"
+  }
+]);
